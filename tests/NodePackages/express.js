d62d10470f193cfc5c713d11b244544970a2f818
AndrewTillott <andytillott@hotmail.co.uk>
2020-08-30 21:19:30 +0100

    express route paramaters

--- tests/NodePackages/express.js
+++ tests/NodePackages/express.js
@@ -5,9 +5,10 @@ var express = require('express');
 var app = express();
 
 //using express to set up the server to a port
+//runs app
 app.listen(4000);
 
-//HTTP Methods
+//HTTP Methods (http verbs)
 //All of these are different http methods (the kind of requests we may make)
 //These methods tell the server to respond to the request in a particular way 
 //Then its up to the server to respond as it see's fit 
@@ -23,10 +24,21 @@ app.get('/', function(req, res){
     //this is an express method
     //express identified that it was a string, so we didn't have to express the content type
     res.send('this is the home page');
-})
+});
 
 app.get('/contact', function(req, res){
     res.send('this is the contact page');
+});
+
+//we can access the id on the request object
+//we just need to specify what the paramater is called (id)
+app.get('/profile/:id', function(req, res){
+    //would log the message below including the id at the end
+    res.send('You requested to to see a profile with the id of ' + req.params.id) 
+});
+
+app.get('/profile/:name', function(req, res){
+    res.send('You requested to see a profile with the name of ' + req.params.name);
 })
 
 //Post
